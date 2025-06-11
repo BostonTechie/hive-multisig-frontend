6e00e2d38cac5fef1defa1902e809d13b637bf7b
BostonTechie <andrewurq@gmail.com>
2025-06-11 11:51:46 -0400

    enforced authenticated routes where needed

--- src/auth/Context.tsx
+++ src/auth/Context.tsx
@@ -1,4 +1,10 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
 
 interface User {
   email: string;
@@ -17,13 +23,28 @@ const AuthContext = createContext<AuthContextType | null>(null);
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [user, setUser] = useState<User | null>(null);
+  const [showVerify, setShowVerify] = useState(false);
+
+  useEffect(() => {
+    setShowVerify(false);
+  }, []);
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+      setIsLoggedIn(true);
+    }
+  }, []);
 
   const login = (userData: User) => {
+    localStorage.setItem('user', JSON.stringify(userData));
     setUser(userData);
     setIsLoggedIn(true);
   };
 
   const logout = () => {
+    localStorage.removeItem('user');
     setUser(null);
     setIsLoggedIn(false);
   };
--- src/components/navigating/Routing.tsx
+++ src/components/navigating/Routing.tsx
@@ -1,4 +1,5 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
+import { useAuth } from '../../auth/Context';
 import Hivelogin from '../page/HiveLogin';
 import { Landing } from '../page/Landing';
 import { DarkGridAuth } from '../page/MyPage';
@@ -11,20 +12,53 @@ import { TransactionPage } from '../page/TransactionsPage';
 import { TwoFactorAuthPage } from '../page/TwoFactorAuthPage';
 import UnderConstruction from './UnderConst';
 
+const AuthenticatedRoute = ({ element }: { element: JSX.Element }) => {
+  const { isLoggedIn } = useAuth();
+  return isLoggedIn ? element : <Navigate to="/landing" />;
+};
+
 const Routing = () => {
   return (
     <div>
       <Routes>
-        <Route path="/:id" element={<HomePage />} />
-        <Route path="/dashboard" element={<SigDashBoard />} />
-        <Route path="/my" element={<DarkGridAuth />} />
-        <Route path="/admin" element={<SigVaultAdmin />} />
-        <Route path="/sig" element={<SigHome />} />
-        <Route path="/search" element={<SearchBar />} />
+        <Route
+          path="/:id"
+          element={<AuthenticatedRoute element={<HomePage />} />}
+        />
+        <Route
+          path="/dashboard"
+          element={<AuthenticatedRoute element={<SigDashBoard />} />}
+        />
+        <Route
+          path="/my"
+          element={<AuthenticatedRoute element={<DarkGridAuth />} />}
+        />{' '}
+        <Route
+          path="/admin"
+          element={<AuthenticatedRoute element={<SigVaultAdmin />} />}
+        />
+        <Route
+          path="/sig"
+          element={<AuthenticatedRoute element={<SigHome />} />}
+        />
+        <Route
+          path="/search"
+          element={<AuthenticatedRoute element={<SearchBar />} />}
+        />
+        <Route
+          path="/transaction"
+          element={<AuthenticatedRoute element={<TransactionPage />} />}
+        />
+        <Route
+          path="/signRequest"
+          element={<AuthenticatedRoute element={<SignRequestsPage />} />}
+        />
+        <Route
+          path="/twoFactor"
+          element={<AuthenticatedRoute element={<TwoFactorAuthPage />} />}
+        />
+        {/* no auth routes */}
         <Route path="/login" element={<Hivelogin />} />
-        <Route path="/transaction" element={<TransactionPage />} />
-        <Route path="/signRequest" element={<SignRequestsPage />} />
-        <Route path="/twoFactor" element={<TwoFactorAuthPage />} />
         <Route path="/landing" element={<Landing />} />
         <Route path="/" element={<Navigate to="/landing" replace />} />
         <Route path="*" element={<UnderConstruction />} />
